4a7be2495c37d274c3177f3206d5d8853b4a70c8
DZariusz <dzariusz@gmail.com>
2020-12-07 10:23:59 +0100

    feat: add contract registry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {LeafType} from './models/LeafType';
 import {LeafKeyCoder} from './services/LeafKeyCoder';
 import {LeafValueCoder} from './services/LeafValueCoder';
+import {Registry as ContractRegistry} from './contracts/Registry';
 
-export {LeafType, LeafValueCoder, LeafKeyCoder}
+export {LeafType, LeafValueCoder, LeafKeyCoder, ContractRegistry}
