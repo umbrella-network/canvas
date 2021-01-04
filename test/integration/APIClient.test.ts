2f5ac264bb99cd039f5e3bb6f501379a858138f5
Edgar <starguitar307@gmail.com>
2021-01-04 18:17:06 +0200

    test(api-client): run verifyProofForBlock tests only if required envs are set

--- test/integration/APIClient.test.ts
+++ test/integration/APIClient.test.ts
@@ -1,24 +1,16 @@
 import dotenv from 'dotenv';
-import {APIClient, LeafType} from '../../src';
-import {expect} from 'chai';
+import { APIClient, LeafType } from '../../src';
+import { expect } from 'chai';
 import { ethers } from 'ethers';
 import { Registry } from '../../src/contracts/Registry';
 import { ChainContract } from '../../src/contracts/Chain';
 
 dotenv.config();
- 
+
 if (process.env.API_BASE_URL) {
   describe('APIClient()', async () => {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.BLOCKCHAIN_PROVIDER_URL || 'ws://127.0.0.1:8545');
-    const registry = new Registry(provider, process.env.REGISTRY_CONTRACT_ADDRESS as string);
-
-    const chainContractAddress = await registry.getAddress('Chain');
-
-    const chainContract = new ChainContract(provider, chainContractAddress);
-
     const apiClient = new APIClient({
       baseURL: process.env.API_BASE_URL as string,
-      chainContract,
     });
 
     // These are integration tests that do not require api key
@@ -74,13 +66,12 @@ if (process.env.API_BASE_URL) {
       });
     });
 
-
     describe('#getProofs', () => {
       it('expect to return a valid result', async () => {
         const keysObjects = await apiClient.getKeys();
 
         // we will pass all the keys, so we could find some proofs for sure
-        const keys = keysObjects.map(keyObject => keyObject.id);
+        const keys = keysObjects.map((keyObject) => keyObject.id);
         const proofs = await apiClient.getProofs(keys);
 
         expect(proofs).be.an('object');
@@ -90,18 +81,46 @@ if (process.env.API_BASE_URL) {
       });
     });
 
-    describe('#verifyProofForBlock', () => {
-      it('expect to work', async () => {
-        const verificationResult = await apiClient.verifyProofForBlock(
-          'eth-usd',
-          LeafType.TYPE_INTEGER
-        );
+    if (
+      process.env.BLOCKCHAIN_PROVIDER_URL &&
+      process.env.REGISTRY_CONTRACT_ADDRESS
+    ) {
+      const provider = new ethers.providers.JsonRpcProvider(
+        process.env.BLOCKCHAIN_PROVIDER_URL || 'ws://127.0.0.1:8545'
+      );
+      const registry = new Registry(
+        provider,
+        process.env.REGISTRY_CONTRACT_ADDRESS as string
+      );
 
-        expect(verificationResult).be.an('object');
-        expect(verificationResult).to.have.property('success').that.is.a('boolean');
-        expect(verificationResult).to.have.property('value');
+      const chainContractAddress = await registry.getAddress('Chain');
+
+      const chainContract = new ChainContract(provider, chainContractAddress);
+
+      const apiClient = new APIClient({
+        baseURL: process.env.API_BASE_URL as string,
+        chainContract,
       });
-    });
+
+      describe('#verifyProofForBlock', () => {
+        it('expect to work', async () => {
+          const verificationResult = await apiClient.verifyProofForBlock(
+            'eth-usd',
+            LeafType.TYPE_INTEGER
+          );
+
+          expect(verificationResult).be.an('object');
+          expect(verificationResult)
+            .to.have.property('success')
+            .that.is.a('boolean');
+          expect(verificationResult).to.have.property('value');
+        });
+      });
+    } else {
+      console.warn(
+        'Skipping ClientAPI integration tests requiring BLOCKCHAIN_PROVIDER_URL and REGISTRY_CONTRACT_ADDRESS, as they are not provided.'
+      );
+    }
 
     if (process.env.API_KEY) {
       // Here we'll write integration tests for methods, that use API-key-protected APIs
