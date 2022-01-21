635285c489e8eecb839b0a20cc61b4d4e2b841c1
Pedro Ramos <pedro.henrique@codeminer42.com>
2022-01-21 15:41:52 -0300

    Update test RPCs

--- test/services/RPCSelector.test.ts
+++ test/services/RPCSelector.test.ts
@@ -28,8 +28,8 @@ describe('RPCSelector', () => {
     describe('when the preferred URL is up to date', () => {
       it('returns the preferred URL', async () => {
         const urls = [
-          'https://data-seed-prebsc-1-s1.binance.org:8545/',
-          'https://data-seed-prebsc-1-s3.binance.org:8545/',
+          'https://data-seed-prebsc-1-s2.binance.org:8545/',
+          'https://data-seed-prebsc-2-s3.binance.org:8545/',
         ];
         const rpcSelector = new RPCSelector(urls);
         expect(await rpcSelector.apply()).to.eq(urls[0]);
@@ -39,8 +39,8 @@ describe('RPCSelector', () => {
     describe('when the preferred URL is not up to date', () => {
       it('returns the RPC with highest block number', async () => {
         const urls = [
-          'https://data-seed-prebsc-2-s1.binance.org:8545/',
-          'https://data-seed-prebsc-1-s1.binance.org:8545/',
+          'https://data-seed-prebsc-2-s3.binance.org:8545/',
+          'https://data-seed-prebsc-1-s2.binance.org:8545/',
         ];
         const rpcSelector = new RPCSelector(urls);
         expect(await rpcSelector.apply()).to.eq(urls[1]);
