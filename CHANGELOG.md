453dc765487da2f9aa40ababf2f6e6fd8d33a421
Pedro Ramos <pedro.henrique@codeminer42.com>
2022-03-04 11:41:20 -0300

    release: v5.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+## [5.7.0] - 2022-02-16
+### Added
+- Support for signed numbers
+- Passport interface, ABI, and Structs
+
+### Changed
+- Updated RPCSelector to throw when no healthy RPC is found
+
 ## [5.6.1] - 2022-02-07
 ### Changed
 - Updated RPCSelector Timestamp strategy to not take too much time to run
---
+++ test/contracts/ExampleContract.test.ts
@@ -0,0 +1,49 @@
+import 'hardhat';
+import '@nomiclabs/hardhat-ethers';
+import { expect, use } from 'chai';
+import { waffleChai } from '@ethereum-waffle/chai';
+import { BigNumber, Contract } from 'ethers';
+import * as hre from 'hardhat';
+const ethers = hre.ethers;
+import * as fs from 'fs';
+
+use(waffleChai);
+
+const setup = async () => {
+  if (!fs.readdirSync('./src/contracts/examples').includes('artifacts')) {
+    await hre.run('compile');
+  }
+  const ExampleContract = await ethers.getContractFactory('ExampleContract');
+  const exampleContract = await ExampleContract.deploy();
+  await exampleContract.deployed();
+  return {
+    exampleContract,
+  };
+};
+
+describe('ExampleContract', () => {
+  let exampleContract: Contract;
+
+  before(async () => {
+    return ({ exampleContract } = await setup());
+  });
+
+  describe('convertUintToInt()', async () => {
+    const maxUint224 = BigNumber.from('0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
+    const maxInt224 = maxUint224.sub(1).div(2);
+    const minInt224 = maxInt224.add(1).mul(-1);
+
+    const testCases = [
+      { input: 0, expected: 0 },
+      { input: 1, expected: 1 },
+      { input: maxUint224.add(1).div(2).toString(), expected: minInt224 },
+      { input: maxUint224.toString(), expected: -1 },
+    ];
+
+    testCases.forEach(({ input, expected }) => {
+      it(`testToInt(${input}) => ${expected}`, async () => {
+        expect(await exampleContract.convertUintToInt(input)).to.eq(expected);
+      });
+    });
+  });
+});
